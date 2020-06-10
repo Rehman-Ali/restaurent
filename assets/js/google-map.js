d48322e7cacad63e986e4630c6d6724315f7cc4a
rehmanali <rehmanali1477@gmail.com>
2020-06-10 15:56:31 +0500

    full-updated

--- assets/js/google-map.js
+++ assets/js/google-map.js
@@ -5,7 +5,10 @@ function init() {
     // Basic options for a simple Google Map
     // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
     // var myLatlng = new google.maps.LatLng(40.71751, -73.990922);
-    var myLatlng = new google.maps.LatLng(40.69847032728747, -73.9514422416687);
+    var myLatlng = new google.maps.LatLng(-37.783544, 144.977060);
+     
+
+
     // 39.399872
     // -8.224454
     
